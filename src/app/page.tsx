e5348824d478d0784612100cefaa00d9be1e2073
Diar <diar@teradata.co.id>
2025-09-26 14:53:33 +0700

    Darkmode System

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import { DarkModeToggle } from '@/components/commons/dark-mode-toggle';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
 export default function Home() {
   return (
     <div>
-      <Input></Input>
+      <Input />
       <Button>Hello</Button>
+      <DarkModeToggle />
     </div>
   );
 }
