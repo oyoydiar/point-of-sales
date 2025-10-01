476709ad49380362bc3c99de04afd0ff986be079
Diar <diar@teradata.co.id>
2025-10-01 17:58:33 +0700

    Sidebar Menu Part 1

--- src/components/commons/app-sidebar.tsx
+++ src/components/commons/app-sidebar.tsx
@@ -3,7 +3,10 @@
 import { EllipsisVertical, LogOut, Store } from 'lucide-react';
 import {
   Sidebar,
+  SidebarContent,
   SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
@@ -21,31 +24,68 @@ import {
 } from '../ui/dropdown-menu';
 import { Avatar, AvatarFallback } from '../ui/avatar';
 import { AvatarImage } from '@radix-ui/react-avatar';
+import { SIDEBAR_MENU_LIST } from '@/constants/sidebar-constant';
+import { usePathname } from 'next/navigation';
 
 export default function AppSidebar() {
   const { isMobile } = useSidebar();
+  const pathname = usePathname();
+  const profile = {
+    name: 'Avip Syaifulloh',
+    role: 'admin',
+    avatar_url: '',
+  };
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon">
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <div className="font-semibold">
                 <div className="bg-teal-500 flex p-2 items-center justify-center rounded-md">
-                  <Store className="size-4" />
+                  <Coffee className="size-4" />
                 </div>
-                Point of Sales
+                WPU Cafe
               </div>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent className="flex flex-col gap-2">
+            <SidebarMenu>
+              {SIDEBAR_MENU_LIST[
+                profile.role as 'admin' | 'cashier' | 'kitchen'
+              ]?.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild tooltip={item.title}>
+                    <a
+                      href={item.url}
+                      className={cn('px-4 py-3 h-auto', {
+                        'bg-teal-500 text-white hover:bg-teal-500 hover:text-white':
+                          pathname === item.url,
+                      })}
+                    >
+                      {item.icon && <item.icon />}
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
           <SidebarMenuItem>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <SidebarMenuButton size="lg">
+                <SidebarMenuButton
+                  size="lg"
+                  className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                >
                   <Avatar className="h-8 w-8 rounded-lg">
                     <AvatarImage src="" alt="" />
                     <AvatarFallback className="rounded-lg">A</AvatarFallback>
