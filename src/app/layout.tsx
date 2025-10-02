65d232f6bcaaad57c4e5e3c560e4b7785ba71306
Diar <diar@teradata.co.id>
2025-10-02 16:31:50 +0700

    State Management Zustand

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/providers/theme-provider';
+import AuthStoreProvider from '@/providers/auth-store-provider';
+import { cookies } from 'next/headers';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -12,24 +14,29 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookiesStore = await cookies();
+  const profile = JSON.parse(cookiesStore.get('user_profile')?.value ?? '{}');
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <AuthStoreProvider profile={profile}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </AuthStoreProvider>
       </body>
     </html>
   );
--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -11,3 +11,10 @@ export type AuthFormState = {
     _form?: string[];
   };
 };
+
+export type Profile = {
+  id?: string;
+  name?: string;
+  avatar_url?: string;
+  role?: string;
+};
