f304bf56004ca9e337709cc8bcc05398d4d3b97a
Diar <diar@teradata.co.id>
2025-10-22 17:37:39 +0700

    Create Order

--- src/app/(dashboard)/order/_components/order.tsx
+++ src/app/(dashboard)/order/_components/order.tsx
@@ -13,6 +13,7 @@ import useDataTable from '@/hooks/use-data-table';
 import { createClient } from '@/lib/supabase/client';
 import { cn } from '@/lib/utils';
 import { HEADER_TABLE_ORDER } from '@/constants/order-constant';
+import DialogCreateOrder from './dialog-create-order';
 
 export default function OrderManagement() {
   const supabase = createClient();
@@ -55,6 +56,19 @@ export default function OrderManagement() {
         });
 
       return result;
+    },
+  });
+
+  const { data: tables, refetch: refetchTables } = useQuery({
+    queryKey: ['tables'],
+    queryFn: async () => {
+      const result = await supabase
+        .from('tables')
+        .select('*')
+        .order('created_at')
+        .order('status');
+
+      return result.data;
     },
   });
 
@@ -108,6 +122,7 @@ export default function OrderManagement() {
             <DialogTrigger asChild>
               <Button variant="outline">Create</Button>
             </DialogTrigger>
+            <DialogCreateOrder tables={tables} refetch={refetch} />
           </Dialog>
         </div>
       </div>
