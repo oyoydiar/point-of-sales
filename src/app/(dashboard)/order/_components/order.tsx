56040930be3aa4f4c9bfa9c3e857539e42ab0b7e
Diar <diar@teradata.co.id>
2025-10-23 15:22:33 +0700

    Handle Reservation

--- src/app/(dashboard)/order/_components/order.tsx
+++ src/app/(dashboard)/order/_components/order.tsx
@@ -1,19 +1,29 @@
 'use client';
 
 import DataTable from '@/components/commons/data-table';
+import DropdownAction from '@/components/commons/dropdown-action';
+import { Button } from '@/components/ui/button';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { useMemo, useState } from 'react';
-import DropdownAction from '@/components/commons/dropdown-action';
-import { Table } from '@/validations/table-validation';
-import { toast } from 'sonner';
-import { useQuery } from '@tanstack/react-query';
 import useDataTable from '@/hooks/use-data-table';
 import { createClient } from '@/lib/supabase/client';
+import { useQuery } from '@tanstack/react-query';
+import { Ban, Link2Icon, ScrollText } from 'lucide-react';
+import {
+  startTransition,
+  useActionState,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import { toast } from 'sonner';
 import { cn } from '@/lib/utils';
+import { Table } from '@/validations/table-validation';
 import { HEADER_TABLE_ORDER } from '@/constants/order-constant';
 import DialogCreateOrder from './dialog-create-order';
+import { updateReservation } from '../actions';
+import { INITIAL_STATE_ACTION } from '@/constants/general-constant';
+import Link from 'next/link';
 
 export default function OrderManagement() {
   const supabase = createClient();
@@ -25,6 +35,7 @@ export default function OrderManagement() {
     handleChangeLimit,
     handleChangeSearch,
   } = useDataTable();
+
   const {
     data: orders,
     isLoading,
@@ -37,21 +48,22 @@ export default function OrderManagement() {
         .select(
           `
             id, order_id, customer_name, status, payment_url, tables (name, id)
-          `,
+            `,
           { count: 'exact' }
         )
         .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
-        .order('created_at', { ascending: false });
+        .order('created_at');
 
-      if (currentSearch.trim())
+      if (currentSearch) {
         query.or(
-          `order_id.ilike.%${currentSearch}%,customer_name.ilike.%${currentSearch}%,status.ilike.%${currentSearch}%`
+          `order_id.ilike.%${currentSearch}%,customer_name.ilike.%${currentSearch}%`
         );
+      }
 
       const result = await query;
 
       if (result.error)
-        toast.error('Get Order   data failed', {
+        toast.error('Get Order data failed', {
           description: result.error.message,
         });
 
@@ -81,6 +93,73 @@ export default function OrderManagement() {
     if (!open) setSelectedAction(null);
   };
 
+  const totalPages = useMemo(() => {
+    return orders && orders.count !== null
+      ? Math.ceil(orders.count / currentLimit)
+      : 0;
+  }, [orders]);
+
+  const [reservedState, reservedAction] = useActionState(
+    updateReservation,
+    INITIAL_STATE_ACTION
+  );
+
+  const handleReservation = async ({
+    id,
+    table_id,
+    status,
+  }: {
+    id: string;
+    table_id: string;
+    status: string;
+  }) => {
+    const formData = new FormData();
+    Object.entries({ id, table_id, status }).forEach(([Key, value]) => {
+      formData.append(Key, value);
+    });
+    startTransition(() => {
+      reservedAction(formData);
+    });
+  };
+
+  useEffect(() => {
+    if (reservedState?.status === 'error') {
+      toast.error('Update Reservation Failed', {
+        description: reservedState.errors?._form?.[0],
+      });
+    }
+
+    if (reservedState?.status === 'success') {
+      toast.success('Update Reservation Success');
+      refetch();
+    }
+  }, [reservedState]);
+
+  const reservedActionList = [
+    {
+      label: (
+        <span className="flex items-center gap-2">
+          <Link2Icon />
+          Process
+        </span>
+      ),
+      action: (id: string, table_id: string) => {
+        handleReservation({ id, table_id, status: 'process' });
+      },
+    },
+    {
+      label: (
+        <span className="flex items-center gap-2">
+          <Ban className="text-red-500" />
+          Cancel
+        </span>
+      ),
+      action: (id: string, table_id: string) => {
+        handleReservation({ id, table_id, status: 'canceled' });
+      },
+    },
+  ];
+
   const filteredData = useMemo(() => {
     return (orders?.data || []).map((order, index) => {
       return [
@@ -98,15 +177,35 @@ export default function OrderManagement() {
         >
           {order.status}
         </div>,
-        <DropdownAction menu={[]} />,
+        <DropdownAction
+          menu={
+            order.status === 'reserved'
+              ? reservedActionList.map((item) => ({
+                  label: item.label,
+                  action: () =>
+                    item.action(
+                      order.id,
+                      (order.tables as unknown as { id: string }).id
+                    ),
+                }))
+              : [
+                  {
+                    label: (
+                      <Link
+                        href={`/order/${order.order_id}`}
+                        className="flex items-center gap-2"
+                      >
+                        <ScrollText />
+                        Detail
+                      </Link>
+                    ),
+                    type: 'link',
+                  },
+                ]
+          }
+        />,
       ];
     });
-  }, [orders]);
-
-  const totalPages = useMemo(() => {
-    return orders && orders.count !== null
-      ? Math.ceil(orders.count / currentLimit)
-      : 0;
   }, [orders]);
 
   return (
@@ -115,7 +214,7 @@ export default function OrderManagement() {
         <h1 className="text-2xl font-bold">Order Management</h1>
         <div className="flex gap-2">
           <Input
-            placeholder="Search order..."
+            placeholder="Search..."
             onChange={(e) => handleChangeSearch(e.target.value)}
           />
           <Dialog>
